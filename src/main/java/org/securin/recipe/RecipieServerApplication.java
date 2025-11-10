efdb446dec9f10be2ccfbb7fa729bf89308fb199
Jefino9488 <jefinojacob9488@gmail.com>
2025-11-10 11:28:41 +0530

    Add Maven wrapper scripts and project setup files

---
+++ src/main/java/org/securin/recipe/RecipieServerApplication.java
@@ -0,0 +1,13 @@
+package org.securin.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipieServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipieServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/securin/recipe/RecipieServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.securin.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipieServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
